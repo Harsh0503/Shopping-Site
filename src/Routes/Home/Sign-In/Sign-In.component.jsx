d2b5b744718778d80cc3e42546d59f8e27cc1cce
Harsh0503 <harshpan78669@gmail.com>
2023-07-10 18:05:16 +0530

    Sign-In

--- src/Routes/Home/Sign-In/Sign-In.component.jsx
+++ src/Routes/Home/Sign-In/Sign-In.component.jsx
@@ -1,14 +1,15 @@
-import {signInWithGooglePopup} from '../../../utils/firebase.utils'
+import {createUserDocumentFromAuth,signInWithGooglePopup} from '../../../utils/firebase.utils'
 
 const SignIn =() => {
     const logGoggleUser = async () =>{
-        const response = await signInWithGooglePopup();
-        console.log(response);
+       
+        const {user} = await signInWithGooglePopup();
+        createUserDocumentFromAuth(user)
     }
     return(
         <div>
             <h1>This is Sign-In Page </h1>
-            <button onClick={logGoggleUser} >Sign In with the google popup </button>
+            <button onClick={logGoggleUser} >Sign In with the google </button>
         </div>
     )
 }
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -2,6 +2,8 @@ import { initializeApp } from "firebase/app"
 
 import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth'
 
+import { getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDpCPGXzILjIG_cGsJtb0JkK6gjW8x9qvU",
@@ -24,3 +26,15 @@ provider.setCustomParameters({
 export const auth = getAuth()
 
 export const signInWithGooglePopup =() => signInWithPopup(auth,provider) ;
+
+export const db = new getFirestore()
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+   const userDocRef = doc(db , 'users' , userAuth.uid )
+
+   console.log(userDocRef)
+
+   const userSnapshot = await getDoc(userDocRef)
+   console.log(userSnapshot)
+   console.log(userSnapshot.exists())
+}
